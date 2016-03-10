c3b95275e110f57be62c46f2f0dca73e7e4971b5
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-03-10 12:04:23 +0900

    add xxz command to myscriptes

---
+++ myscripts/.myscripts/xxz/source/app.d
@@ -0,0 +1,32 @@
+import std.process,
+       std.stdio,
+       std.conv,
+       std.file,
+       std.path;
+
+void xxz(string fileName) {
+  writeln("Extract tar package " ~ fileName ~ " into " ~ baseName(fileName, ".xz"));
+  executeShell("tar -xf " ~ fileName);
+  writeln("Decompress " ~ baseName(fileName, ".xz") ~ " into " ~ baseName(fileName, ".tar.xz") ~ " with xz");
+  executeShell("xz -x" ~ baseName(fileName, ".xz"));
+  writeln("Remove tar package" ~ baseName(fileName, ".xz"));
+  executeShell("rm " ~ baseName(fileName, "xz"));
+}
+
+void main(string[] args) {
+  args = args[1..$];
+  if (args.length < 1) {
+    throw new Error("Too few arguments");
+  }
+
+  foreach (arg; args) {
+    if (exists(arg)) {
+      xxz(arg);
+    } else {
+      writeln("Not found such a file", arg);
+      continue;
+    }
+  }
+}
+
+
